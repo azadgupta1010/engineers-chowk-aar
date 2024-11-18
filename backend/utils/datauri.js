a3add86e7096146ad9f7ae747f1b38220b2e8b7d
azadgupta1010 <147590498+azadgupta1010@users.noreply.github.com>
2024-11-18 21:06:14 +0530

    Update datauri.js

--- backend/utils/datauri.js
+++ backend/utils/datauri.js
@@ -1,3 +1,4 @@
+/*
 import DataUriParser from "datauri/parser.js";
 import path from "path";
 
@@ -5,6 +6,26 @@ const getDataUri = (file) => {
     const parser = new DataUriParser();
     const extName = path.extname(file.originalname).toString();
     return parser.format(extName,file.buffer);
+}
+
+export default getDataUri;
+*/
+import DataUriParser from "datauri/parser.js";
+import path from "path";
+
+const getDataUri = (file) => {
+    if (!file || !file.originalname || !file.buffer) {
+        throw new Error("Invalid file input. File must have 'originalname' and 'buffer' properties.");
+    }
+
+    const parser = new DataUriParser();
+    const extName = path.extname(file.originalname).toString();
+
+    if (!extName) {
+        throw new Error("File does not have a valid extension.");
+    }
+
+    return parser.format(extName, file.buffer);
 }
 
 export default getDataUri;
